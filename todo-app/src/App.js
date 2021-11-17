aa6376ca7f3d2fdf18f05afd8f948487e4d6423a
kskish <kskish18@gmail.com>
2021-11-16 20:23:05 -0500

    Filter Todos

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
@@ -6,6 +6,34 @@ import TodoList from "./components/TodoList";
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filterTodos, setFilterTodos] = useState([]);
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilterTodos(
+          todos.filter((todo) => {
+            return todo.completed === true;
+          })
+        );
+        break;
+      case "uncompleted":
+        setFilterTodos(
+          todos.filter((todo) => {
+            return todo.completed === false;
+          })
+        );
+        break;
+      default:
+        setFilterTodos(todos);
+        break;
+    }
+  };
 
   return (
     <div className="App">
@@ -17,8 +45,14 @@ function App() {
         setInputText={setInputText}
         todos={todos}
         setTodos={setTodos}
+        setStatus={setStatus}
       />
-      <TodoList inputText={inputText} todos={todos} setTodos={setTodos} />
+      <TodoList
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        filterTodos={filterTodos}
+      />
     </div>
   );
 }
